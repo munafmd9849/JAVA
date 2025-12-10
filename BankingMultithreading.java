e9506490747fb0ff69822378997bd59c8b377776
Munaf <191462807+munafmd9849@users.noreply.github.com>
2025-12-10 23:51:53 +0530

    Initialize BankingMultithreading with main method

---
+++ BankingMultithreading.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) throws Exception{
+	    Scanner scan=new Scanner(System.in);
+	    System.out.println("Banking Activity Started");
+	    System.out.println("Enter the Account Numberfff");
+	    
+	    
+	}
+}
+
